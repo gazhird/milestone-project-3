d626bc03f61569cb41a6537b5e4bc0145a6dc636
gazhird <gazhird@live.co.uk>
2025-11-10 12:06:00 +0000

    test

---
+++ wheels/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path
+from listings import views
+
+urlpatterns = [
+    path('', views.test, name='test'),
+    path('admin/', admin.site.urls),
+]
