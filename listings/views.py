8b4862a25ddc1c76d1ca15c21b7f173b38513ea9
gazhird <gazhird@live.co.uk>
2025-12-19 11:19:41 +0000

    editing navbar

--- listings/views.py
+++ listings/views.py
@@ -8,9 +8,18 @@ class ListingList(generic.ListView):
     context_object_name = 'listings'
     template_name = 'listings/listing_list.html'
 
+    # search query
     def get_queryset(self):
-        queryset = Listing.objects.order_by('make')   # start with everything
+        queryset = Listing.objects.order_by('make') 
         make = self.request.GET.get('make', '').strip()
         if make:
             queryset = queryset.filter(make__icontains=make)
         return queryset
+    
+    # all vehicles
+    def get_context_data(self, **kwargs):
+        fullset = super().get_context_data(**kwargs)
+        fullset['all_listings'] = Listing.objects.order_by('make')  
+        return fullset
+    
+    
